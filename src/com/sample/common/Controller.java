966c11004ab535a570e37bb560d162e75824c6a4
hdpark0620 <sksgmlejr@gmail.com>
2019-10-14 10:56:50 +0900

    first commit

---
+++ src/com/sample/common/Controller.java
@@ -0,0 +1,14 @@
+package com.sample.common;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame; 
+
+public abstract class Controller extends JFrame implements ActionListener {
+
+	/**
+	 * serialVersionUID
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
